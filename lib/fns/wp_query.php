45325a5d521c19f9564c8d403b84900c656efef3
Michael Wender <michael@michaelwender.com>
2021-10-18 17:32:13 -0400

    Changing from reference to value
    Fixing this error: `Parameter 2 to contentXpressImporter\lib\fns\wp_query\title_like_posts_where() expected to be a reference, value given`

--- lib/fns/wp_query.php
+++ lib/fns/wp_query.php
@@ -15,7 +15,7 @@ namespace contentXpressImporter\lib\fns\wp_query;
  * @param obj $wp_query WP_Query object.
  * @return str Modified MySQL `WHERE` clause.
  */
-function title_like_posts_where( $where, &$wp_query ) {
+function title_like_posts_where( $where, $wp_query ) {
     global $wpdb;
     if ( $post_title_like = $wp_query->get( 'post_title_like' ) ) {
         $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql( $wpdb->esc_like( $post_title_like ) ) . '%\'';
